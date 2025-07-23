a780eb1bbb12137c289cfd89dfd9a25f21c50784
kristy-zhj <936115905@qq.com>
2025-07-23 10:44:20 +0800

    🚧 screenshot

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,84 @@
 import ReactLogo from "@assets/react.svg"
 import { Button, Modal, Spin } from "antd"
 import csn from "classnames"
-import html2canvas from "html2canvas"
 import { useAtom } from "jotai"
 import { useRef, useState } from "react"
 import { ThemeModeAtom } from "./main"
 import "./index.css"
+import { AlignCenterOutlined } from "@ant-design/icons"
+import html2canvas from "html2canvas-pro"
+import { domToPng } from "modern-screenshot"
 
 function App() {
   const elementRef = useRef<HTMLDivElement>(null)
-  const [blob, setBlob] = useState<Blob | null>(null)
+  const [blob, setBlob] = useState("")
   const [modalOpen, setModalOpen] = useState<boolean>(false)
   const [themeMode, setThemeMode] = useAtom(ThemeModeAtom)
 
+  const onScreenshot1 = () => {
+    const element = elementRef?.current
+    if (!element) return console.log("element is null")
+    domToPng(element, { scale: 2 })
+      .then(dataUrl => {
+        setBlob(dataUrl)
+        setModalOpen(true)
+      })
+      .catch(error => {
+        console.error("截图失败:", error)
+      })
+  }
   const onScreenshot = () => {
     const element = elementRef?.current
-
-    if (!element) return
-    html2canvas(element, {
-      useCORS: true,
-      scale: 2,
-    }).then(canvas => {
-      canvas.toBlob(blob => {
-        if (blob) {
-          setBlob(blob)
-          setModalOpen(true)
-        }
-      }, "image/png")
-    })
+    if (!element) return console.log("element is null")
+    html2canvas(element, { scale: 2 })
+      .then(data => {
+        setBlob(data.toDataURL())
+        setModalOpen(true)
+      })
+      .catch(error => {
+        console.error("截图失败:", error)
+      })
   }
 
   const close = () => {
     setModalOpen(false)
-    setBlob(null)
+    setBlob("")
   }
 
   const toggleTheme = () => {
     const theme = themeMode === "light" ? "dark" : "light"
     setThemeMode(theme)
-    document.documentElement.classList.value = theme
+    document.documentElement.classList = theme
   }
 
   return (
     <div className={csn("p-4 text-center", themeMode === "dark" ? "darkMode " : "")}>
-      <div ref={elementRef} className={csn("p-4 text-center", themeMode === "dark" ? "darkMode " : "")}>
+      <div
+        ref={elementRef}
+        className={csn("p-4 text-center w-[500px] mx-auto", themeMode === "dark" ? "dark " : "")}
+      >
         <div className="flex justify-center items-center gap-2 mb-4 h-9">
-          <Button type="default">你好</Button>
-          <Button type="primary" className="h-9 leading-9" onClick={toggleTheme}>
+          <Button type="primary">Primary Button</Button>
+          <Button
+            type="primary"
+            className="flex justify-center items-center !text-base"
+            onClick={toggleTheme}
+          >
             切换主题
+            <AlignCenterOutlined />
           </Button>
-          <Button type="primary" className="h-9 leading-9" onClick={onScreenshot}>
+          <Button type="default" className="h-9 leading-9" onClick={onScreenshot}>
             截图
           </Button>
+          <div className="bg-black/30 w-20 h-20 overflow-y-auto">
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+          </div>
         </div>
         <div className="flex justify-center items-center">
           <ReactLogo />
@@ -77,12 +104,12 @@ function App() {
             <div className="w-full h-[80vh]">
               <img
                 className="border border-borderColor border-solid w-full object-contain"
-                src={URL.createObjectURL(blob)}
-                alt={""}
+                src={blob}
+                alt="截图预览"
               />
             </div>
           ) : (
-            <Spin />
+            <Spin className="flex justify-center" />
           )}
         </div>
       </Modal>
